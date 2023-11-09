3fed70ef2c7015d3c07525d7c69309b5bd061917
davidgb8246 <85250523+davidgb8246@users.noreply.github.com>
2023-11-09 10:53:56 +0100

    Update actualizarArchivo-infoAlumnos.sh

--- actualizarArchivo-infoAlumnos.sh
+++ actualizarArchivo-infoAlumnos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SOLICITAMOS AL USUARIO EL NOMBRE DEL ARCHIVO DONDE ESTÁ ALMACENANDO LA INFORMACIÓN DE LOS ALUMNOS PARA ACTUALIZARLO
-echo "Dime el nombre del achivo donde se almacena la información de los alumnos para actualizarlo: "
+echo "Dime el nombre del achivo donde se almacena la información de los alumnos para actualizarlo desde el repositorio principal: "
 read fileName
 
 #OBTENDRÉMOS EL ARCHIVO DEL REPOSITORIO PRINCIPAL Y LO ACTUALIZAREMOS
