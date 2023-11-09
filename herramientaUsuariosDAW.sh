401fdcba1ce2c4e1657a1929e10eabcb1cdfda81
davidgb8246 <85250523+davidgb8246@users.noreply.github.com>
2023-11-09 10:40:46 +0100

    Update herramientaUsuariosDAW.sh

--- herramientaUsuariosDAW.sh
+++ herramientaUsuariosDAW.sh
@@ -3,9 +3,13 @@
 #CREAREMOS LA CARPETA DONDE ALMACENAREMOS LAS HERRAMIENTAS:
 mkdir herramientaUsuariosDAW
 
+#SOLICITAREMOS AL USUARIO EL NOMBRE DEL ARCHIVO DONDE SE ALMACENARÁN LA INFORMACIÓN DE LOS ALUMNOS.
+echo "Dime el nombre del archivo para almacenar la información de los alumnos."
+read alumnosFileName
+
 #DESCARGAREMOS LAS HERRAMIENTAS Y EL ARCHIVO DE REFERENCIA:
 echo "Descargando las herramientas y el archivo de referencia..."
-curl -s https://raw.githubusercontent.com/davidgb8246/practicaSistemas/main/infoAlumnos > herramientaUsuariosDAW/infoAlumnos
+curl -s https://raw.githubusercontent.com/davidgb8246/practicaSistemas/main/infoAlumnos > herramientaUsuariosDAW/$alumnosFileName
 curl -s https://raw.githubusercontent.com/davidgb8246/practicaSistemas/main/crearAlumnosPrimeroDAW.sh > herramientaUsuariosDAW/crearAlumnosPrimeroDAW.sh
 curl -s https://raw.githubusercontent.com/davidgb8246/practicaSistemas/main/borrarAlumnosPrimeroDAW.sh > herramientaUsuariosDAW/borrarAlumnosPrimeroDAW.sh
 curl -s https://raw.githubusercontent.com/davidgb8246/practicaSistemas/main/actualizarArchivo-infoAlumnos.sh > herramientaUsuariosDAW/actualizarArchivo-infoAlumnos.sh
