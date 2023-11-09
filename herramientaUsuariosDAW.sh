133a2b8cc4a103c39decbde5627ba9bee6b4c1f6
davidgb8246 <85250523+davidgb8246@users.noreply.github.com>
2023-11-09 10:15:06 +0100

    Add files via upload

---
+++ herramientaUsuariosDAW.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#CREAREMOS LA CARPETA DONDE ALMACENAREMOS LAS HERRAMIENTAS:
+mkdir herramientaUsuariosDAW
+
+#DESCARGAREMOS LAS HERRAMIENTAS Y EL ARCHIVO DE REFERENCIA:
+echo "Descargando las herramientas y el archivo de referencia..."
+curl -s https://raw.githubusercontent.com/davidgb8246/practicaSistemas/main/infoAlumnos > herramientaUsuariosDAW/infoAlumnos
+curl -s https://raw.githubusercontent.com/davidgb8246/practicaSistemas/main/crearAlumnosPrimeroDAW.sh > herramientaUsuariosDAW/crearAlumnosPrimeroDAW.sh
+curl -s https://raw.githubusercontent.com/davidgb8246/practicaSistemas/main/borrarAlumnosPrimeroDAW.sh > herramientaUsuariosDAW/borrarAlumnosPrimeroDAW.sh
+
+#DAREMOS PERMISOS DE EJECUCIÓN A LOS SCRIPTS:
+chmod ug+x herramientaUsuariosDAW/crearAlumnosPrimeroDAW.sh herramientaUsuariosDAW/borrarAlumnosPrimeroDAW.sh
